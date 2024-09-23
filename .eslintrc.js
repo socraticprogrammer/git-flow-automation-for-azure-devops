1e08248d6627042d744250ab37b5492ee4cfe6c5
socraticprogrammer <thallesgabriel1307@gmail.com>
2024-09-23 14:13:23 -0300

    chore: :package: integrate prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,14 @@
 module.exports = {
   root: true,
-  plugins: ["@typescript-eslint", "import"],
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
   extends: [
-    "airbnb-typescript/base",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/typescript",
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: "./tsconfig.json",
+    project: './tsconfig.json',
   },
 };
